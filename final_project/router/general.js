9e558e391dabee1a887a470686f64fdd54f65b31
Navya Chikkabyranna <50332565+Navyacb@users.noreply.github.com>
2024-10-21 22:25:28 +0200

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,19 +19,53 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn; // Extract the ISBN from the request parameters
+  const book = books[isbn]; // Access the book using the ISBN as the key
+
+  if (book) {
+    return res.status(200).json(book); // Return the book details if found
+  } else {
+    return res.status(404).json({ message: "Book not found" }); // Return an error if not found
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author; // Extract the author from the request parameters
+  const booksByAuthor = [];
+
+  for (const key in books) {
+    if (books[key].author.toLowerCase() === author.toLowerCase()) {
+      booksByAuthor.push(books[key]);
+    }
+  }
+
+  if (booksByAuthor.length > 0) {
+    return res.status(200).json(booksByAuthor); // Return the list of books by the author
+  } else {
+    return res.status(404).json({ message: "No books found by this author" });
+  }
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title; // Extract the title from the request parameters
+  const booksByTitle = [];
+
+  for (const key in books) {
+    if (books[key].title.toLowerCase() === title.toLowerCase()) {
+      booksByTitle.push(books[key]);
+    }
+  }
+
+  if (booksByTitle.length > 0) {
+    return res.status(200).json(booksByTitle); // Return the list of books with the given title
+  } else {
+    return res.status(404).json({ message: "No books found with this title" });
+  }
 });
 
 //  Get book review
