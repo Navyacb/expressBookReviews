a6c8c9d5d9f501b283811e02e1fd0b49a579e99b
Navya Chikkabyranna <50332565+Navyacb@users.noreply.github.com>
2024-10-22 14:34:36 +0200

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,23 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if both username and password are provided
+    if (username && password) {
+        // Check if the user does not already exist
+        if (!isValid(username)) {
+            // Add the new user to the users array
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
@@ -71,7 +84,15 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn; // Extract the ISBN from the request parameters
+    const book = books[isbn]; // Access the book using the ISBN as the key
+  
+    if (book) {
+      const reviews = book.reviews; // Get the reviews for the book
+      return res.status(200).json(reviews); // Return the reviews
+    } else {
+      return res.status(404).json({ message: "Book not found" }); // Return an error if the book is not found
+    }
 });
 
 module.exports.general = public_users;
